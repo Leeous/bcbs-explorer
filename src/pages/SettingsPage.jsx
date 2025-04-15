b3af243ba16540e003103556304ecaf3350b5a9b
Cody Fields (Leeous) <contact@leeous.com>
2025-04-15 17:17:52 -0400

    map localStorage to savedNotes

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,5 +1,7 @@
 import Navigation from "../components/Navigation";
 
+let savedNotes = Object.keys(localStorage);
+
 function clearNoteStorage() {
   // Clear storage
   localStorage.clear();
@@ -8,31 +10,30 @@ function clearNoteStorage() {
 }
 
 function Settings() {
-    return (
-        <>
-          <Navigation />
-            <details className="settings-cat">
-                <summary>Appearance</summary>
-                
-            </details>
-            <details className="settings-cat">
-                <summary>Saved Notes</summary>
-                <div>
-                  {Object.keys(localStorage).map(key => (
-                    <>
-                      <h4 key={localStorage[key]}>{key}</h4>
-                      <p key={key} style={{fontSize: ".9rem"}} className="noteText">{localStorage[key]}</p>
-                    </>
-                  ))}
-            </div>
-            </details>
+  return (
+      <>
+        <Navigation />
+          <details className="settings-cat">
+              <summary>Appearance</summary>
+              
+          </details>
+          <details className="settings-cat">
+              <summary>Saved Notes</summary>
+              <div>
+                {savedNotes.map(key => (
+                  <>
+                    <h4 key={localStorage[key]}>{key}</h4>
+                    <p key={key} style={{fontSize: ".9rem"}} className="noteText">{localStorage[key]}</p>
+                  </>
+                ))}
+          </div>
+          </details>
 
-            <button className="button-normal">Reset all</button>
-            <button className="button-normal" onClick={() => clearNoteStorage()}>Reset notes</button>
-            <button className="button-normal">Reset settings</button>
-            
-        </>
-    );
+          <button className="button-normal">Reset all</button>
+          <button className="button-normal" onClick={clearNoteStorage}>Reset notes</button>
+          <button className="button-normal">Reset settings</button>
+      </>
+  );
 }
 
 // TODO: add abbirution to settings page - <a target="_blank" href="https://icons8.com/icon/59782/error">Warning</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
