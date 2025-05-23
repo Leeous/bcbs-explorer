399f040ca080d72883e0b380eb89d42027141751
Cody Fields (Leeous) <contact@leeous.com>
2025-05-23 19:31:59 -0400

    remove universal-cookie, use localStorage instead (why was I doing that 🤔)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
-import Disclaimer from './components/Disclaimer';
 import NotFound404 from "./pages/404";
 import SearchPage from './pages/SearchPage';
 import SettingsPage from './pages/SettingsPage';
 import AddCarrier from './pages/AddCarrier';
-import Cookies from 'universal-cookie';
 import './assets/css/App.css';
-
-const cookies = new Cookies();
-
-const disclaimerAccepted = () => {
-  if (cookies.get("disclaimerAccepted")) {
-    return <SearchPage />
-  } else {
-    return <Disclaimer />
-  }
-}
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
         <Route path="/search" element={ <SearchPage /> } />
-        <Route path="/" element={ disclaimerAccepted() } />
+        <Route path="/" element={ <SearchPage /> } />
         <Route path="/settings" element={ <SettingsPage /> } />
         <Route path="/add" element={ <AddCarrier /> } />
-        <Route path="/disclaimer" element={ <Disclaimer /> } />
         <Route path="/404" element={ <NotFound404/>} />
-        <Route path="/index.html" element={disclaimerAccepted()} />
         <Route path="*" element={<Navigate replace to="/search" />} />
       </Routes>
     </BrowserRouter>
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState, useRef, useCallback } from 'react';
 import BCBSDB from "../assets/bcbs_data.json";
 import CarrierCard from "../components/CarrierCard";
-import Note from "../components/Note"
+import Note from "../components/Note";
+import Modal from '../components/Modal';
 import Navigation from '../components/Navigation';
 
 const Search = () => {
@@ -24,7 +25,7 @@ const Search = () => {
       planName: key,
       ...BCBSDB[key]
     }));
-    
+
     let prefixMatch = searchType == "prefix" ? carriers.find(carrier => carrier.prefixes.includes(value.toLowerCase())) : null;
     let carrierMatch = searchType == "carrier" ? carriers.filter(carrier => (carrier.planName.toLowerCase().includes(value.toLowerCase()))) : null;
 
@@ -44,7 +45,7 @@ const Search = () => {
   }, [maxLength, searchType]);
 
   useEffect(() => { handleSearch(searchValue); }
-  , [searchValue, handleSearch]);
+    , [searchValue, handleSearch]);
 
   const handleCarrierSelection = (e) => {
     let eTarget = e.target;
@@ -73,7 +74,7 @@ const Search = () => {
       setResults([]);
     } else {
       setSearchValue(event.target.value);
-    } 
+    }
   }
 
   const handleSearchClick = () => {
@@ -85,6 +86,39 @@ const Search = () => {
 
   return (
     <>
+      <Modal title="DISCLAIMER" cookieName="disclaimerAck" showRememberCheckbox={false} confirmationText="I understand and agree" description={`
+          <div className="disclaimerModal">
+            This browser extension, BCBS Explorer, is designed to provide users with information about Blue Cross Blue Shield (BCBS) carriers. The information provided by this extension is for general informational purposes only and is not intended to be a substitute for official documentation.
+            <br/>
+            <br/>
+            <h3>Accuracy of Information</h3>
+            While I strive to ensure that the information provided by this extension is accurate and up-to-date, I make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the information, products, services, or related graphics contained in the extension for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+            <br/>
+            <br>
+            <h3>No Endorsement</h3>
+            This extension is not affiliated with, endorsed by, or sponsored by any Blue Cross Blue Shield carrier or any of its affiliates. The use of any trade names, trademarks, service marks, logos, or domain names (collectively “Marks”) in this extension does not imply any affiliation with or endorsement by the respective owners.
+            <br/>
+            <br/>
+            <h3>Limitation of Liability</h3>
+            In no event will I be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this extension.
+            <br/>
+            <br/>
+            <h3>External Links</h3>
+            Through this extension, you may be able to link to other websites which are not under the control of Leeous&apos; Creations. I have no control over the nature, content, and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+            <br/>
+            <br/>
+            <h3>Changes to This Disclaimer</h3>
+            I may update this disclaimer from time to time. I will notify you of any changes by posting the new disclaimer on this page. You are advised to review this disclaimer periodically for any changes. Changes to this disclaimer are effective when they are posted on this page.
+            <br/>
+            <br/>
+            <h3>Notes fields</h3>
+            Notes are not currently encrypted - please do not store PHI in this field as it will not be properly secured.
+            <br/>
+            <br/>
+            <h3>Contact Us</h3>
+            If you have any questions about this disclaimer, contact me at <span><a href="mailto:contact@leeous.com">contact@leeous.com</a>.</span>
+          </div>`} />
+    
       <Navigation />
       <div className='search-wrapper'>
         <section>
